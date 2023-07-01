ea9d7cbdf8c429954235419cfa9a563a915f75aa
Dror Atariah <drorata@gmail.com>
2023-07-01 10:34:25 +0200

    Extracted fixtures to module

--- foo_hardware/tests/test_user.py
+++ foo_hardware/tests/test_user.py
@@ -1,48 +1,10 @@
-import pytest
 from fastapi.testclient import TestClient
-from sqlmodel import Session, SQLModel, create_engine
-from sqlmodel.pool import StaticPool
-
-from foo_hardware import models
-from foo_hardware.backend import app, get_session
-
-
-@pytest.fixture(name="session")
-def session_fixture():
-    engine = create_engine(
-        "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
-    )
-    SQLModel.metadata.create_all(engine)
-    with Session(engine) as session:
-        yield session  #
-
-
-@pytest.fixture(name="client")
-def client_fixture(session: Session):
-    def get_session_override():
-        return session
-
-    app.dependency_overrides[get_session] = get_session_override
-
-    client = TestClient(app)
-    yield client
-    app.dependency_overrides.clear()
-
-
-@pytest.fixture(name="bootstrap_data")
-def bootstrap_data_fixture(client: TestClient, session: Session):
-    session.add(
-        models.User(
-            username="Peter Pen", password="pass123", email="peter@neverland.io"
-        )
-    )
-    session.add(
-        models.User(
-            username="Captain Hook", password="pass456", email="hook@neverland.io"
-        )
-    )
-    session.commit()
-    yield {"client": client, "session": session}
+from foo_hardware.backend import app
+from foo_hardware.tests.fixtures import (  # noqa: F401
+    session_fixture,
+    client_fixture,
+    bootstrap_data_fixture,
+)
 
 
 def test_root_of_app():
@@ -54,9 +16,6 @@ def test_root_of_app():
 def test_get_empty_users(client: TestClient):
     response = client.post("/user/", json={})
     app.dependency_overrides.clear()
-
-    print(response.json())
-
     assert response.status_code == 422
 
 
